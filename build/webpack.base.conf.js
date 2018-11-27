63cbe4ec112bd594c49c31028b4bfaf4a50aa48a
Victor Silva <victor.silva@wearecactus.com>
2018-11-27 19:22:12 +0100

    svg logo

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -47,6 +47,10 @@ module.exports = {
         options: vueLoaderConfig
       },
       {
+        test: /\.svg$/,
+        use: [{ loader: 'html-loader' }]
+      },  
+      {
         test: /\.js$/,
         loader: 'babel-loader',
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="main">
+        <img src="@/assets/cactus_logo.svg" width="20%" alt="">
+        <p>Inserta tu nombre</p>
+        <input type="text">
+    </div>
+</template>
+<script>
+export default {
+}
+</script>
